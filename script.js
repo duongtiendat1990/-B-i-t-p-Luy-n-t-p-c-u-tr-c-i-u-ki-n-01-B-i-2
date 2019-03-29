af8e6292521bc27a2ef12f99972ba42862394d9a
duongtiendat1990 <48908567+duongtiendat1990@users.noreply.github.com>
2019-03-29 14:44:16 +0700

    'Bài 2'

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+function listen() {
+    document.addEventListener('keydown', checkKey);
+}
+function checkKey(e) {
+    var keyCode = (event.key);
+    if (keyCode == '+' || keyCode == '-' || keyCode == '.') {
+        e.preventDefault()
+    }
+}
 function Check() {
     let age = document.getElementById("age").value;
     if (age<=3||age>185) {
